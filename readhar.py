0353a2471ac01a70f6bd8348f759b94f3029144d
Hans Gmelig Meyling <hans@meylingmedia.nl>
2022-05-31 10:46:57 +0200

    Create readhar.py

---
+++ readhar.py
@@ -0,0 +1,18 @@
+# Get Urls from HAR file, collect then sort and print out
+
+import json
+
+urls = []
+with open("web-dej.hec.stedingroep.nl.har", "r") as response_f:
+    response_dict = json.load(response_f)
+    logentries = response_dict["log"]["entries"]
+    for entry in logentries:
+        for line in logentries:
+            url = line["request"]["method"] + ":" + line["request"]["url"]
+            if not url in urls:
+                urls.append(url)
+            #print(line["request"]["method"], line["request"]["url"])
+
+urls.sort()
+for url in urls:
+    print(url)
