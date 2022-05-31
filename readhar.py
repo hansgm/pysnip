9961f56775c137ecbd100de8dfbaf59c6bf6dc13
Hans Gmelig Meyling <hans@meylingmedia.nl>
2022-05-31 10:51:45 +0200

    Update readhar.py

--- readhar.py
+++ readhar.py
@@ -6,11 +6,10 @@ urls = []
 with open("sitename.har", "r") as response_f:
     response_dict = json.load(response_f)
     logentries = response_dict["log"]["entries"]
-    for entry in logentries:
-        for line in logentries:
-            url = line["request"]["method"] + ":" + line["request"]["url"]
-            if not url in urls:
-                urls.append(url)
+    for line in logentries:
+        url = line["request"]["method"] + ":" + line["request"]["url"]
+        if not url in urls:
+            urls.append(url)
             #print(line["request"]["method"], line["request"]["url"])
 
 urls.sort()
